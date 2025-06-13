74b98ea41111716d20ad2a1397c08940ec0f316c
Guillaume AGNIERAY <dev@agnieray.net>
2025-06-13 11:42:00 +0200

    Bump version

--- _define.php
+++ _define.php
@@ -25,10 +25,10 @@ $this->register(
     'Galette Legal Notices',
     'Manage legal notices in Galette',
     'Guillaume AGNIERAY',
-    '1.0.0-rc6',
+    '1.0.0-rc7',
     '1.2.0',
     'legalnotices',
-    '2025-05-01',
+    '2025-06-13',
     [
         'legalnotices_settings' => 'admin',
         'legalnotices_store_settings' => 'admin',
